5218bc96d5d1b801d7afe41b6f10f6164b523d02
mahadirazib <31607628+mahadirazib@users.noreply.github.com>
2022-11-20 20:53:48 +0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,96 @@
+
+
+
+let addToCart = (el)=>{
+    let ele = el.parentElement.parentElement.getElementsByClassName("productText");
+    let name = ele[0].getElementsByClassName("productName")[0].innerText;
+    let price = ele[0].getElementsByClassName("productPrice")[0].innerText
+    let tableRow = document.createElement("tr");
+    
+    tableRow.innerHTML = "<td>" + name + "</td>" + "<td class='cartPrice'>" + price.slice(0, -1) + "</td>" + "<td onclick='removeProduct(this)' class='crossButton'> X </td>";
+
+    let parent = document.querySelector(".cartList");
+    parent.appendChild(tableRow)
+
+    calculatePrice();
+    console.log("Product added successfully");
+}
+
+
+
+
+
+
+function seeCart(ele){
+    document.querySelector(".productContainer").style.width = "70%";
+    document.querySelector(".productContainer").style.margin = "20px";
+    document.querySelector(".cartDetails").style.opacity = "1";
+    document.querySelector(".cartDetails").style.zIndex = "1";
+    ele.innerText = "➡";    
+    ele.setAttribute("onclick", "unseenCart(this)");
+}
+
+function unseenCart(ele){
+    document.querySelector(".productContainer").style.width = "100%";
+    document.querySelector(".productContainer").style.margin = "20px auto";
+    document.querySelector(".cartDetails").style.opacity = "0";
+    document.querySelector(".cartDetails").style.zIndex = "-1";
+    ele.innerText = "⬅";
+    ele.setAttribute("onclick", "seeCart(this)");
+}
+
+
+
+
+
+
+
+
+let num = document.querySelectorAll(".product").length;
+
+for(let i=0; i<num; i++){
+    document.querySelectorAll(".product")[i].addEventListener("mouseenter", function(){
+        this.getElementsByClassName("imageContainer")[0].style.height = "130px";
+        this.getElementsByClassName("addToCartButton")[0].style.zIndex = "2";
+        this.getElementsByClassName("addToCartButton")[0].style.opacity = "1";
+    })
+    
+    document.querySelectorAll(".product")[i].addEventListener("mouseleave", function(){
+        this.getElementsByClassName("imageContainer")[0].style.height = "200px";
+        this.getElementsByClassName("addToCartButton")[0].style.zIndex = "-1";
+        this.getElementsByClassName("addToCartButton")[0].style.opacity = "0";
+    })
+}
+
+
+
+
+
+
+
+function removeProduct(ele){
+    let parent = ele.parentElement.parentElement;
+    parent.removeChild(ele.parentElement);
+    calculatePrice();
+    console.log("Product removed successfully");
+}
+
+
+
+
+
+
+function calculatePrice(){
+    let num = document.querySelectorAll(".cartPrice").length;
+    let sum = 0;
+    let ele;
+    for(let i=0; i<num; i++){
+        ele = document.querySelectorAll(".cartPrice")[i].innerText;
+        sum += Number(ele);
+    }
+
+    document.querySelector("#showPrice").innerHTML = sum;
+}
+
+
+
